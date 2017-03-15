78d73bb0a0b1a1f3099d7f4f84265bee530d7786
Sean Ellison-Chen <nihilates@gmail.com>
2017-03-14 21:12:26 -0400

    added universal port to server

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get('*', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-
+var port = process.env.PORT || 8080;
 //Establish listening
-app.listen(8080);
-console.log("The Hearth is Roaring on port 8080");
+app.listen(port);
+console.log("The Hearth is Roaring on port "+port);
